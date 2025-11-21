e3fafb2cd97a4db2ad12ab4c825970264f2f7669
flowlitesystems-creator <tu_correo_de_github>
2025-11-20 19:21:26 -0500

    webhook step 3

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,23 @@
 from flask import Flask, request, jsonify
+from os import getenv
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def home():
     return "FlowLite OS1 funcionando en Render."
 
-# ===== WEBHOOK GREENAPI =====
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    data = request.json
-    print("WEBHOOK RECIBIDO:", data)  # Para ver el JSON en Render Logs
-    return jsonify({"status": "ok"})
+    try:
+        data = request.json
+
+        print("📩 WEBHOOK RECIBIDO:")
+        print(json.dumps(data, indent=4, ensure_ascii=False))
+
+        return jsonify({"status": "ok"}), 200
+
+    except Exception as e:
+        print("❌ ERROR WEBHOOK:", e)
+        return jsonify({"error": str(e)}), 500
